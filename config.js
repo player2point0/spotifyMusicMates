fe2e7e5ff5b2b9f652655ca10c0f41042f76f878
nathan <nathannash11@gmail.com>
2019-04-07 19:39:56 +0100

    removed api key

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const config = {
       port: 8888
     },
     spotify:{
-        my_client_id : "672aa60cb7aa4372905a8806f8411f9d",
-        my_client_secret : "b91edfdc85864773aefd4bf8bcce77aa",
+        my_client_id : "",
+        my_client_secret : "",
         redirect_uri : 'http://localhost:8888/callback',
         scopes : 'user-read-recently-played user-read-currently-playing playlist-modify-public user-library-read'
     }
